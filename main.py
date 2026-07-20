2233aa8dbabbcadd2b314496ef4ddc7d9ca60d31
Beyza Hız <beyzahiz.03@gmail.com>
2026-07-20 23:41:13 +0300

    sentence vs sentences

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
 model = SentenceTransformer('all-MiniLM-L6-v2')
 # SentenceTransformer bir class
 # model bu classtan üretilmiş bir nesne
 
 sentence = "Kediler süt içer."
 embedding = model.encode(sentence)
-print(embedding)
+#print(embedding)
+
+sentences = [
+    "Kediler süt içer.",
+    "Köpekler kemik yemeyi sever.",
+    "Python popüler bir programlama dilidir.",
+    "Yapay zeka sağlık alanında kullanılmaktadır.",
+    "Kanser tedavisinde immünoterapi umut veriyor."
+]
+
+embeddings = model.encode(sentences)
+
+
